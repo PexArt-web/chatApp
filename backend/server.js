473ab522535446d4f6123bac7ae94528e0190961
PexArt-web <pexart74@gmail.com>
2024-10-27 13:12:05 +0100

    weBSocket

--- backend/server.js
+++ backend/server.js
@@ -5,4 +5,28 @@ const app   = express();
 const port = process.env.PORT || 4000
 const server = app.listen(port, ()=> log("chat server launched at port " + port) );
 
-app.use(express.static(path.join(__dirname, "public",)))
+const io =  require('socket.io')(server);
+
+app.use(express.static("public"))
+
+// 
+
+let socketConnected = new Set()
+
+io.on("connection", onConnect)
+
+function onConnect(socket) {
+    log(socket.id)
+    io.emit("clients-total", socketConnected.size)
+    socketConnected.add(socket.id)
+
+    socket.on("disconnect",()=>{
+        log("disconnected", socket.id)
+        socketConnected.delete(socket.id)
+    })
+
+    socket.on("message", (data)=>{
+        socket.broadcast.emit("message", data)
+        log("received message", data)
+    })
+}
